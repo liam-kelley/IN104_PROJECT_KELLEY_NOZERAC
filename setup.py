5edec24a018b157cacfc32349fca45c23ad7ff01
Nozerac Gabriel <gabriel.nozerac@ensta-paris.fr>
2021-05-21 20:22:10 +0200

    init.py update

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 
+from minimaxIterDeepBrain import MinimaxIterDeepBrain as default_AI
+
 setup(name='IN104_PROJECT_KELLEY_NOZERAC',
       description='IN104 Project package',
       author='KELLEY_NOZERAC',
       packages=find_packages()
       )
+
